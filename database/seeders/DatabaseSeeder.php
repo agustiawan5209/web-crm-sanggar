5c7869e215f1907140a22b920f9fccd0c2deec8d
Agustiawan <44895587+agustiawan5209@users.noreply.github.com>
2024-07-12 19:34:48 +0800

    buat get and keep diskon

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\GetDiskon;
+use App\Models\KeepDiskon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        GetDiskon::create(['min_quantity'=> '10']);
+        KeepDiskon::create(['min_frequency'=> '3']);
         $this->call([
             PermissionSeeder::class,
             RoleSeeder::class,
