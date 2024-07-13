6b66f64cbeb71f9981cd12756c5956003d2ae706
Agustiawan <44895587+agustiawan5209@users.noreply.github.com>
2024-07-14 03:13:01 +0800

    buat model dan controller penyewaan

---
+++ app/Http/Controllers/PembayaranController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pembayaran;
+use App\Http\Requests\StorePembayaranRequest;
+use App\Http\Requests\UpdatePembayaranRequest;
+use App\Models\Penyewaan;
+use Illuminate\Support\Facades\Request;
+use Inertia\Inertia;
+
+class PembayaranController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tableName = 'penyewaans'; // Ganti dengan nama tabel yang Anda inginkan
+        // $columns = DB::getSchemaBuilder()->getColumnListing($tableName);
+        $columns[] = 'id';
+        $columns[] = 'customer_id';
+        $columns[] = 'produk';
+        $columns[] = 'tgl_pengambilan';
+        $columns[] = 'tgl_pengembalian';
+        $columns[] = 'status';
+
+        return Inertia::render('Admin/Penyewaan/Index', [
+            'search' =>  Request::input('search'),
+            'table_colums' => array_values(array_diff($columns, ['remember_token', 'password', 'email_verified_at', 'created_at', 'updated_at', 'user_id', 'deskripsi'])),
+            'data' => Penyewaan::paginate(10),
+            'can' => [
+                'add' => true,
+                'edit' => true,
+                'show' => true,
+                'delete' => true,
+                'reset_password' => true,
+            ]
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePembayaranRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pembayaran $pembayaran)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pembayaran $pembayaran)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePembayaranRequest $request, Pembayaran $pembayaran)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pembayaran $pembayaran)
+    {
+        //
+    }
+}
