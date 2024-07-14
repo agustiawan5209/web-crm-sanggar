fd9145d425564c42220111d221e3f2737354db1e
Agustiawan <44895587+agustiawan5209@users.noreply.github.com>
2024-07-15 07:40:55 +0800

    buat file untuk router customer

---
+++ routes/customer.php
@@ -0,0 +1,40 @@
+<?php
+use App\Http\Controllers\Customer\CustomerController;
+use App\Http\Controllers\Customer\PembayaranController as CustomerPembayaranController;
+use App\Http\Controllers\Customer\PenyewaanController as CustomerPenyewaanController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Payment\PembayaranController;
+use App\Http\Controllers\PenyewaanController;
+
+Route::middleware(['auth', 'verified', 'role:Customer'])->group(function () {
+    // Dashboard User
+    Route::group(['prefix'=> 'user', 'as'=> 'Customer.'], function () {
+        Route::controller(CustomerController::class)->group(function () {
+            Route::get('/dashboard', 'index')->name('dashboard');
+            Route::get('/profile', 'profile')->name('profile');
+        });
+        // Penyewaan Jasa Barang
+        Route::controller(CustomerPenyewaanController::class)->group(function () {
+            Route::get('/penyewaan', 'index')->name('penyewaan');
+        });
+
+
+        // Data Pembayaran User
+        Route::group(['prefix' => 'pembayaran', 'as' => "Pembayaran."], function () {
+            Route::controller(CustomerPembayaranController::class)->group(function () {
+                Route::get('/', 'index')->name('index');
+                Route::get('/detail-pembayaran', 'show')->name('show');
+            });
+        });
+    });
+    // End Dashboard
+
+    // Pembayaran COntroller
+    Route::controller(PembayaranController::class)->group(function () {
+        Route::get('/checkout-penyewaan', 'index')->name('payment.checkout');
+        Route::get('/success-penyewaan', 'success')->name('payment.success');
+    });
+
+    // Buat Data Penyewaan
+    Route::post('store-penyewaan', [PenyewaanController::class, 'store'])->name('Penyewaan.Store');
+});
